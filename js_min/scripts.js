7d6555e5b52bfb77b38f43b041eb9068b1e13225
Антон Цык <tsyk.a@extmedia.by>
2017-09-30 21:09:38 +0300

    arrows

--- js_min/scripts.js
+++ js_min/scripts.js
@@ -155,7 +155,60 @@ $(document).ready(function () {
             speed: 1000,
             dots: true
         });
+        $('.dostavka .items').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            /*autoplay: true,
+            autoplaySpeed: 1000,
+            speed: 1000,*/
+            dots: true
+        });
     }
+    $('.slider_mobile').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 750,
+                settings: {
+                    arrows: true,
+                    dots: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $('.brend .items').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1160,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
     $('.autoplay').slick({
         slidesToShow: 3,
         slidesToScroll: 3,
